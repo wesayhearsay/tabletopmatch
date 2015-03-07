969c621a05facee839d7fad1c88bce36016b7482
lipcanu <diana.lipcanu@gmail.com>
2015-03-07 19:59:30 +0100

    model changed

--- js/model.js
+++ js/model.js
@@ -1,15 +1,337 @@
 var game_tags = {
-	"themes" : [{name :'abstract',	selected: 'false'},
-				{name: 'fantasy',	selected: 'false'},
-				{name: 'sci fi',	selected: 'false'},
-				{name: 'train',		selected: 'false'}],
-	"categories" : [{name :'board game',	selected: 'false'},
-					{name: 'dice game',	selected: 'false'},
-					{name: 'card game',	selected: 'false'},
-					{name: 'miniature game',		selected: 'false'}],
-	"mechanics" : [{name :'area control',	selected: 'false'},
-					{name: 'deck building',	selected: 'false'},
-					{name: 'deduction',	selected: 'false'}],
+
+	"type" : [{name :'Board game',	selected: 'false'},
+				{name: 'Card game',	selected: 'false'},
+				{name: 'Dice game',	selected: 'false'},
+				{name: 'Miniature waregame',		selected: 'false'},
+				{name: 'Paper & pencil',		selected: 'false'},
+				{name: 'Tile based',		selected: 'false'}],
+	"themes" : [
+  {
+    "name":"Rail / Network",
+    "selected":"false"
+  },
+  {
+    "name":"Economic simulation",
+    "selected":"false"
+  },
+  {
+    "name":"Environmental",
+    "selected":"false"
+  },
+  {
+    "name":"Educational",
+    "selected":"false"
+  },
+  {
+    "name":"Wargame",
+    "selected":"false"
+  },
+  {
+    "name":"Fantasy",
+    "selected":"false"
+  },
+  {
+    "name":"Historical",
+    "selected":"false"
+  },
+  {
+    "name":"Pre-historic",
+    "selected":"false"
+  },
+  {
+    "name":"Ancient",
+    "selected":"false"
+  },
+  {
+    "name":"Medieval",
+    "selected":"false"
+  },
+  {
+    "name":"20th century",
+    "selected":"false"
+  },
+  {
+    "name":"Renaissance",
+    "selected":"false"
+  },
+  {
+    "name":"Murder / mistery",
+    "selected":"false"
+  },
+  {
+    "name":"Horror",
+    "selected":"false"
+  },
+  {
+    "name":"Religion",
+    "selected":"false"
+  },
+  {
+    "name":"Mythology",
+    "selected":"false"
+  },
+  {
+    "name":"Pirates",
+    "selected":"false"
+  },
+  {
+    "name":"Science fiction",
+    "selected":"false"
+  },
+  {
+    "name":"Space exploration",
+    "selected":"false"
+  },
+  {
+    "name":"Zombies",
+    "selected":"false"
+  },
+  {
+    "name":"Multiple time periods",
+    "selected":"false"
+  },
+  {
+    "name":"Agricol",
+    "selected":"false"
+  },
+  {
+    "name":"Civilization",
+    "selected":"false"
+  },
+  {
+    "name":"Jungle",
+    "selected":"false"
+  },
+  {
+    "name":"abstract",
+    "selected":"false"
+  }
+],
+	"categories" : [
+  {
+    "name":"Adventure",
+    "selected":"false"
+  },
+  {
+    "name":"Strategy",
+    "selected":"false"
+  },
+  {
+    "name":"Race game",
+    "selected":"false"
+  },
+  {
+    "name":"Diplomacy",
+    "selected":"false"
+  },
+  {
+    "name":"Exploration",
+    "selected":"false"
+  },
+  {
+    "name":"Party game",
+    "selected":"false"
+  },
+  {
+    "name":"Word game",
+    "selected":"false"
+  },
+  {
+    "name":"Drinking game",
+    "selected":"false"
+  },
+  {
+    "name":"Family game",
+    "selected":"false"
+  },
+  {
+    "name":"Children game",
+    "selected":"false"
+  },
+  {
+    "name":"Print and play",
+    "selected":"false"
+  }
+],
+	"mechanics" : [
+  {
+    "name":"Area movement",
+    "selected":"false"
+  },
+  {
+    "name":"Grid movement",
+    "selected":"false"
+  },
+  {
+    "name":"Line drawing",
+    "selected":"false"
+  },
+  {
+    "name":"Modular board",
+    "selected":"false"
+  },
+  {
+    "name":"Point to point movement",
+    "selected":"false"
+  },
+  {
+    "name":"Roll and move",
+    "selected":"false"
+  },
+  {
+    "name":"Route / Network Building",
+    "selected":"false"
+  },
+  {
+    "name":"Secret unit deployment",
+    "selected":"false"
+  },
+  {
+    "name":"Tile placement",
+    "selected":"false"
+  },
+  {
+    "name":"Worker placement",
+    "selected":"false"
+  },
+  {
+    "name":"Action programming",
+    "selected":"false"
+  },
+  {
+    "name":"Auction / bidding",
+    "selected":"false"
+  },
+  {
+    "name":"Campaign ",
+    "selected":"false"
+  },
+  {
+    "name":"Betting / Wagering",
+    "selected":"false"
+  },
+  {
+    "name":"Cooperative",
+    "selected":"false"
+  },
+  {
+    "name":"Dice rolling",
+    "selected":"false"
+  },
+  {
+    "name":"Deck building",
+    "selected":"false"
+  },
+  {
+    "name":"Hand management",
+    "selected":"false"
+  },
+  {
+    "name":"Partnerships",
+    "selected":"false"
+  },
+  {
+    "name":"Pattern building",
+    "selected":"false"
+  },
+  {
+    "name":"Pattern recognition",
+    "selected":"false"
+  },
+  {
+    "name":"Player elimination",
+    "selected":"false"
+  },
+  {
+    "name":"Press your luck",
+    "selected":"false"
+  },
+  {
+    "name":"RPG",
+    "selected":"false"
+  },
+  {
+    "name":"Trading",
+    "selected":"false"
+  },
+  {
+    "name":"Trick-taking",
+    "selected":"false"
+  },
+  {
+    "name":"Voting",
+    "selected":"false"
+  },
+  {
+    "name":"Collectible components",
+    "selected":"false"
+  },
+  {
+    "name":"Abstract strategy",
+    "selected":"false"
+  },
+  {
+    "name":"Shedding",
+    "selected":"false"
+  },
+  {
+    "name":"Accumulating ",
+    "selected":"false"
+  },
+  {
+    "name":"Comparing",
+    "selected":"false"
+  },
+  {
+    "name":"Card drafting",
+    "selected":"false"
+  },
+  {
+    "name":"Living card game",
+    "selected":"false"
+  },
+  {
+    "name":"Deduction",
+    "selected":"false"
+  },
+  {
+    "name":"Storytelling",
+    "selected":"false"
+  },
+  {
+    "name":"Memory",
+    "selected":"false"
+  },
+  {
+    "name":"Matching",
+    "selected":"false"
+  },
+  {
+    "name":"Acting",
+    "selected":"false"
+  },
+  {
+    "name":"Negotiation",
+    "selected":"false"
+  },
+  {
+    "name":"Bluffing",
+    "selected":"false"
+  },
+  {
+    "name":"Dexterity",
+    "selected":"false"
+  },
+  {
+    "name":"Trivia",
+    "selected":"false"
+  },
+  {
+    "name":"Area control",
+    "selected":"false"
+  }
+]
 }
 
 var tagsToShow = 3;
