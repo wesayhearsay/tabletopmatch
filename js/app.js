cefdf0ee4544ddad6779d0e3da30488eb670cd3a
lipcanu <diana.lipcanu@gmail.com>
2015-03-07 16:20:49 +0100

    router created

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
 'use strict';
 
 /*main module definition*/
-var ttmatch = angular.module("ttmatchApp", ["ngRoute", 
+var ttmatch = angular.module("ttmatchApp", ["ngRoute", "ngTouch",
     "ttmatchApp.Controllers",  "ttmatchApp.Services", "ttmatchApp.Directives", "ttmatchApp.Filters"]);
+
+// make the views change when the URL is changed aka router 
+
+ttmatch.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+        when('/', {
+            templateUrl: 'partials/firstView.html',
+            controller: 'firstViewController'
+
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+    }
+]);
+ttmatch.config(['$httpProvider',function($httpProvider) {
+      //Enable cross domain calls
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+    }
+]);
