b55363c9f3ac9d397ae2457fadf911f4cdaa87db
lipcanu <diana.lipcanu@gmail.com>
2015-03-08 20:35:46 +0100

    controller adapted to work with new games view

--- js/controller.js
+++ js/controller.js
@@ -10,21 +10,25 @@ controllersModule.controller('playersController', function($scope) {
 controllersModule.controller('gamesController', function($scope, $http, $location, gamesService) {
     //checks if the games.json has ever been accessed before.
     //this is done so that the json is not included every time 
-    // if (!gamesService.isInitialized()) {
-        //if it hasn't been included before, an ajax call is made 
-        //towards the file that is contained in the same folder as index.html
-        $http.get("js/games.json").success(function(data) {
-            //the questionaire variable is set with the data gotten from the call
-            // gamesService.set(data);
-            console.log(data);
-            $scope.games = data;
-            console.log($scope.games);
-        }).
-        error(function() { //if an error occured, the console shows that the json wasn't included
-            console.log("json not included");
-        });
-    // }
-   
+    if (!gamesService.isInitialized()) {
+    //if it hasn't been included before, an ajax call is made 
+    //towards the file that is contained in the same folder as index.html
+    $http.get("js/games.json").success(function(data) {
+        //the questionaire variable is set with the data gotten from the call
+        // gamesService.set(data);
+        console.log(data);
+        $scope.games = data;
+        console.log($scope.games);
+    }).
+    error(function() { //if an error occured, the console shows that the json wasn't included
+        console.log("json not included");
+    });
+    }
+    //if a game is pressed, it routs it to the game view
+    $scope.show = function(gameID){
+      console.log(gameID);
+      $location.path(':'+ gameID);
+    }
 });
 /* Controller for the time filter*/
 controllersModule.controller('timeController', function($scope, $location) {
@@ -71,7 +75,32 @@ controllersModule.controller('filterController', function($scope){
     }
 
 });
-controllersModule.controller('ratingController', function($scope) {
+controllersModule.controller('gameController', function($scope, $http, $routeParams, gamesService) {
+    //gets rid of the : character
+    var $id = $routeParams.gameId.substring(1);
+    //checks if the games.json has ever been accessed before.
+    //this is done so that the json is not included every time the question is changed
+    if (!gamesService.isInitialized()) {
+        //if it hasn't been included before, an ajax call is made 
+        //towards the file that is contained in the same folder as index.html
+        $http.get("js/games.json").success(function(data) {
+            //the questionaire variable is set with the data gotten from the call
+            gamesService.set(data);
+            //if in the path there is an array that stands as a attrID (the question name)
+            if ($routeParams.gameId) {
+                //the current scope gets the array that is associated with that particular question
+                $scope.game = gamesService.get($id);
+            }
+        }).
+        error(function() { //if an error occured, the console shows that the json wasn't included
+            console.log("json not included");
+        });
+    }
+    // in case the games has already been included, and there is an gameID in the route
+    if ($id && gamesService.isInitialized()) {
+        //the current scope gets the array that is associated with that particular question
+        $scope.game = gamesService.get($id);
+    }
     $scope.rate = 7;
     $scope.max = 10;
     $scope.isReadonly = false;
