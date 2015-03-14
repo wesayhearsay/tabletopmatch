0726bb34923d41021f4de7a191d733341a42d195
francesco <info@francescovitale.com>
2015-03-14 18:37:54 +0100

    filter for time filter with hours and minutes

--- js/filters.js
+++ js/filters.js
@@ -1,3 +1,18 @@
 'use strict'
 //define a filters module
 var filtersModule = angular.module("ttmatchApp.Filters", [])
+
+
+filtersModule.filter("timeFilter", function(){
+	// filter by Maxim Shoustin: http://stackoverflow.com/a/25470593
+	return function (time) {
+
+                if (time < 60) {
+                    return (time) + 'm';
+                } else if (time % 60 == 0) {
+                    return (time - time % 60) / 60 + 'h';
+                } else {
+                    return ((time - time % 60) / 60 + 'h' + ' ' + time % 60 + 'm');
+                }
+    };
+})
