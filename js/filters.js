87fa06efe9906b138760e70607623952f2539ecf
francesco <info@francescovitale.com>
2015-03-16 20:31:53 +0100

    filter for complexity in sidebarGameDetails + number of filtered games in content

--- js/filters.js
+++ js/filters.js
@@ -6,13 +6,34 @@ var filtersModule = angular.module("ttmatchApp.Filters", [])
 filtersModule.filter("timeFilter", function(){
 	// filter by Maxim Shoustin: http://stackoverflow.com/a/25470593
 	return function (time) {
-
-                if (time < 60) {
-                    return (time) + 'm';
-                } else if (time % 60 == 0) {
-                    return (time - time % 60) / 60 + 'h';
-                } else {
-                    return ((time - time % 60) / 60 + 'h' + ' ' + time % 60 + 'm');
-                }
+        if (time < 60) {
+            return (time) + 'm';
+        } else if (time % 60 == 0) {
+            return (time - time % 60) / 60 + 'h';
+        } else {
+            return ((time - time % 60) / 60 + 'h' + ' ' + time % 60 + 'm');
+        }
     };
+})
+
+filtersModule.filter("complexityFilter", function(){
+    return function(value){
+        switch(value)
+        {
+            case "1": 
+                return "Easy"
+                break;
+            case "2":
+                return "Medium"
+                break;
+            case "3":
+                return "Complex"
+                break;
+            case "4":
+                return "Complex"
+                break;  
+
+        }
+
+    }
 })
