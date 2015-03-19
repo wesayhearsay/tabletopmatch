a5bba44f911010aa5a59b3c4d43639eeadd1e680
wbock <willie.bock@gmail.com>
2015-03-19 17:45:47 +0100

    Revert "Clean-up controllers and routing"
    This reverts commit dd326721bb2b8074baecc86d67d894c2e26eb563.

---
+++ js/statgraphs.js
@@ -0,0 +1,15 @@
+
+function drawStats(gameLibrary){
+    
+    //console.log(completeLibrary)
+ var svg = d3.select("#profileSidebar").append("svg")
+                .attr("width", 200)
+                .attr("height", 200)
+                .append("g")
+                .attr("transform", "translate(100,100)");
+    svg.append("circle")
+            .attr("cy",0)
+            .attr("cx",0)
+            .attr("r",50)
+            .style("fill", "#1F0A1D");
+}
